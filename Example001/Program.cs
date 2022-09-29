703bd8bcd2506c1776c43c6ebf133417fe4dc03e
Ruslan Karimov <ru-ka-@mail.ru>
2022-09-29 14:01:40 +0500

    added example001

---
+++ Example001/Program.cs
@@ -0,0 +1,73 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int [,] GetArray (int m, int n, int min, int max)
+{
+    int[,] resultArray = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultArray[i, j] = new Random().Next(min, max);
+        }
+    }
+    return resultArray;
+}
+
+int [,] ChangeArrayMaxToMin (int[,] array)
+{
+     for (int i = 0; i < array.GetLength(0); i++)
+     {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j; k < array.GetLength(1); k++)
+            {
+                int tmp = 0;
+                if (array [i,j] < array [i,k])
+                {
+                    tmp = array [i,j];
+                    array [i,j] = array [i,k];
+                    array [i,k] = tmp;
+                }
+            }
+        }
+     }
+     return array;
+}
+
+Console.WriteLine ("Введите число строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите число столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите минимальное число для генерации: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите максимальное число для генерации: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] array = GetArray (m, n, min, max);
+
+Console.WriteLine("Сгенирирован следующий массив: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int [,] newArray = ChangeArrayMaxToMin (array);
+Console.WriteLine("Измененный массив: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < newArray.GetLength(1); j++)
+    {
+        Console.Write(newArray[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
